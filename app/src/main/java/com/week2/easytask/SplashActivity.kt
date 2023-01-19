7dc1671745cb8d8432a33fed05b5d26da83c12bc
박진성 <plashdof@gmail.com>
2023-01-20 04:11:28 +0900

    SplashActivity 구현중

---
+++ app/src/main/java/com/week2/easytask/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.week2.easytask
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },1000)
+
+    }
+}
